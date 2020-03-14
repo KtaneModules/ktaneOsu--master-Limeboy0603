d64c1d3509f2f1994e1492a81a27fe0ee31a7a46
Limeboy0603 <56017871+Limeboy0603@users.noreply.github.com>
2020-03-14 09:42:12 +0800

    Osu! into osu!

--- Assets/Osu.cs
+++ Assets/Osu.cs
@@ -63,7 +63,7 @@ public class Osu : MonoBehaviour
                 while(selectedmapBG == random_mapBG[j])
                     selectedmapBG = Random.Range(0, 50);
             }
-            Debug.LogFormat("[Osu! {0}]Generated map background: {1}", _moduleID ,selectedmapBG);
+            Debug.LogFormat("[osu! {0}]Generated map background: {1}", _moduleID ,selectedmapBG);
         }
         canvas_image.material.mainTexture = BGimage[random_mapBG[0]];
     }
@@ -278,7 +278,7 @@ public class Osu : MonoBehaviour
                 break;
         }
         MapInfo_Map.text = map_song + "\n" + map_author_mapper;
-        Debug.LogFormat("[Osu! {0}] Generated map song: {1}. Author and creator: {2}", _moduleID, map_song, map_author_mapper);
+        Debug.LogFormat("[osu! {0}] Generated map song: {1}. Author and creator: {2}", _moduleID, map_song, map_author_mapper);
     }
 
     void changeBG()
@@ -291,7 +291,7 @@ public class Osu : MonoBehaviour
 
     void submit()
     {
-        Debug.LogFormat("[Osu! {0}]Submitted image: {1}. Correct image: {2}", _moduleID, random_mapBG[canvas_currentimagecounter], random_mapBG[random_mapinfo]);
+        Debug.LogFormat("[osu! {0}]Submitted image: {1}. Correct image: {2}", _moduleID, random_mapBG[canvas_currentimagecounter], random_mapBG[random_mapinfo]);
         if (random_mapBG[canvas_currentimagecounter] == random_mapBG[random_mapinfo])
         {
             Module.HandlePass();
